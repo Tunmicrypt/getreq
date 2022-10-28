7078d56fb9ae4d3400d92655d80409d19bc753a8
tunmicrypt <tunmiay777@gmail.com>
2022-10-28 21:55:17 +0100

    my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains an attempt to use the GET request in nodejs to parse JSON.
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("https");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/", function(req, res){
+    res.json({ "slackUsername": 'Tunmicrypt',
+                    "backend": true,
+                    "age": 21,
+                    "bio": "I am a  back end engineer. My dream is to create something of value to the community"
+                  });
+})
+
+
+app.listen(3000, function(error){
+  if(!error) {console.log("server started at port 3000")}
+  else console.log("error occured");;
+})
